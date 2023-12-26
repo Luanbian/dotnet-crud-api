2386ac39642af8301dbb772212a827cebd959e9d
Luan Bianchini <luanbianchine1@gmail.com>
2023-12-26 18:42:54 -0300

    feat: create usecase create product first version

---
+++ Src/Data/Usecases/CreateProduct.cs
@@ -0,0 +1,19 @@
+﻿using ProductManagement.API.Src.Domain.Entities;
+
+namespace ProductManagement.API.Src.Data.Usecases
+{
+    public class CreateProduct
+    {
+        public Product Perform () 
+        {
+            ProductProps props = new ProductProps () 
+            { 
+                Title = "titulo de teste",
+                Description = "Description teste",
+                Price = 1
+            };
+            Product product = Product.Create (props);
+            return product;
+        }
+    }
+}
