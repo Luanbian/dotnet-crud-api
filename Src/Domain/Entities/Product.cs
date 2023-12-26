3c61e44ce5b6bfff7f95cdde0bd550696cb73148
Luan Bianchini <luanbianchine1@gmail.com>
2023-12-26 18:23:50 -0300

    feat: create product entity and own properties

---
+++ Src/Domain/Entities/Product.cs
@@ -0,0 +1,24 @@
+﻿namespace ProductManagement.API.Src.Domain.Entities
+{
+    public class Product
+    {
+        private Guid Id { get; set; }
+        private string Title { get; set; }
+
+        private string? Description { get; set; }
+
+        private int Price { get; set; }
+
+        private Product (ProductProps props)
+        {
+            Title = props.Title;
+            Description = props.Description;
+            Price = props.Price;
+        }
+
+        public static Product Create(ProductProps props) 
+        { 
+            return new Product (props);
+        }
+    }
+}
---
+++ Src/Domain/Entities/ProductProps.cs
@@ -0,0 +1,11 @@
+﻿namespace ProductManagement.API.Src.Domain.Entities
+{
+    public record ProductProps
+    {
+        public string Title { get; init; }
+
+        public string? Description { get; init; }
+
+        public int Price { get; init; }
+    }
+}
