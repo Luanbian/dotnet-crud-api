7c8227981979e509c9799e7f942462d47a58ee1b
Luan Bianchini <luanbianchine1@gmail.com>
2023-12-27 16:45:16 -0300

    feat: inject usecase in controller to list products

--- Src/Presentation/Controllers/ListProductsController.cs
+++ Src/Presentation/Controllers/ListProductsController.cs
@@ -1,16 +1,21 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using ProductManagement.API.Src.Data.Protocols;
+using ProductManagement.API.Src.Domain.Entities;
 
 namespace ProductManagement.API.Src.Presentation.Controllers
 {
     [Route("api/products")]
     [ApiController]
-    public class ListProductsController : ControllerBase
+    public class ListProductsController(IListAllProductProtocol list) : ControllerBase
     {
+        private readonly IListAllProductProtocol list = list;
+
         [HttpGet]
         public IActionResult GetAll()
         {
-            return Ok("tudo certo");
+            List<Product> products = list.Perform();
+            return Ok(products);
         }
     }
 }
--- obj/Debug/net8.0/ProductManagement.API.AssemblyInfo.cs
+++ obj/Debug/net8.0/ProductManagement.API.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProductManagement.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7358eb5c9fde479ebe26ccd88e9862e7a793eccb")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProductManagement.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProductManagement.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
