6afc6c7ad4bb28ab48f46e055a073cebd9cc51ac
Luan Bianchini <luanbianchine1@gmail.com>
2023-12-27 10:45:23 -0300

    style: format spaces in entity

--- Src/Domain/Entities/Product.cs
+++ Src/Domain/Entities/Product.cs
@@ -6,9 +6,7 @@ namespace ProductManagement.API.Src.Domain.Entities
     {
         public Guid Id { get; set; }
         public string Title { get; set; }
-
         public string? Description { get; set; }
-
         public int Price { get; set; }
 
         private Product (ProductDto props)
