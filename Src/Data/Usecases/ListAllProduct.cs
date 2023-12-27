5b10b10290cfa460c0ac37b55f388315fbdbc417
Luan Bianchini <luanbianchine1@gmail.com>
2023-12-27 16:40:44 -0300

    feat: create list all products use case

---
+++ Src/Data/Usecases/ListAllProduct.cs
@@ -0,0 +1,15 @@
+﻿using ProductManagement.API.Src.Data.Protocols;
+using ProductManagement.API.Src.Domain.Entities;
+using ProductManagement.API.Src.Infra.Repositories.Protocols;
+
+namespace ProductManagement.API.Src.Data.Usecases
+{
+    public class ListAllProduct(IFindRepository<Product> find) : IListAllProductProtocol
+    {
+        private readonly IFindRepository<Product> repository = find;
+        public List<Product> Perform()
+        {
+            return repository.FindAll();
+        }
+    }
+}
