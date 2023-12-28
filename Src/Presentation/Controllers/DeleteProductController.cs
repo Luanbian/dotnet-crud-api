7645d887670ed00c4ff9dbb6b47bcf7b60e04834
Luan Bianchini <luanbianchine1@gmail.com>
2023-12-28 12:54:55 -0300

    feat: implement controller to delete by id

---
+++ Src/Presentation/Controllers/DeleteProductController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ProductManagement.API.Src.Data.Protocols;
+
+namespace ProductManagement.API.Src.Presentation.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DeleteProductController(IDeleteProductProtocol delete) : ControllerBase
+    {
+        private readonly IDeleteProductProtocol delete = delete;
+
+        [HttpDelete]
+        public IActionResult Delete([FromQuery] string id)
+        {
+            delete.Perform(id);
+            return Ok("deletado com sucesso");
+        }
+    }
+}
